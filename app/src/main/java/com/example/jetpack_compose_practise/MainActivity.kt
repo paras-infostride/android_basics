766c74de55e623cfdff8f96228c8fc2c98cd636a
Paras Arora <paras@infostride.com>
2024-05-30 18:15:37 +0530

    added dependency to kick start jetpack compose

---
+++ app/src/main/java/com/example/jetpack_compose_practise/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.jetpack_compose_practise
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpack_compose_practise.ui.theme.Jetpack_compose_practiseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            Jetpack_compose_practiseTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Paras Arora")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Jetpack_compose_practiseTheme {
+        Greeting("Paras Arora")
+    }
+}
